650f98a5f1894e50755d44ac69e2ae69bf2f4212
SUDHARSAN S <sudharsa8850@gmail.com>
2025-06-15 20:42:51 +0530

    added basic color changer functionality using react

---
+++ src/Main.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function Main({ colorName, setColorName }) {
+  let [toggleColor, setToogleColor] = useState("white");
+
+  let [hexColor,setHexColor] = useState("");
+
+  function handleToogle() {
+    toggleColor === "white" ? setToogleColor("black") : setToogleColor("white");
+  }
+
+  function colorNameToHex(colorName) {
+    const ctx = document.createElement("canvas").getContext("2d");
+    ctx.fillStyle = colorName;
+    return ctx.fillStyle;
+  }
+
+  return (
+    <main className="colorInfo">
+      <section
+        className="color"
+        style={{ backgroundColor: colorName, color: toggleColor }}
+      >
+        <div className="colorByName">{colorName}</div>
+        <div className="colorByHex">{hexColor}</div>
+      </section>
+      <form>
+        <input
+          type="text"
+          className="colorName"
+          autoFocus
+          value={colorName}
+          onChange={(e) => {
+            const newColor = e.target.value;
+            setColorName(newColor);
+            setHexColor(()=>colorNameToHex(newColor))
+          }
+        }
+        />
+      </form>
+      <button className="toogle" onClick={handleToogle}>
+        Toggle Text Color
+      </button>
+    </main>
+  );
+}
+
+export default Main;
